83b8493e31ef2f4611b605265f2892de26de899a
isadoraora <isa.mdeo@gmail.com>
2019-10-25 00:56:44 -0200

    inclusao de exercícios feitos do curso de PHP

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+    <title>Página de Teste</title>
+    <link rel="stylesheet" href="style9.css">
+</head>
+
+<body>
+    <header>
+        <h1>Título da Página</h1>
+        <p>Subtítulo</p>
+    </header>
+    <nav>
+        <a href=""></a>
+
+    </nav>
+    <footer>rodapé da página</footer>
+</body>
+
+</html>
